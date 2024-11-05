353c5a6b9791b69b049751480b0d3fafbcbe387d
mohandast52 <mohandast52@gmail.com>
2024-11-05 18:35:03 +0530

    feat: add NoAvailableSlotsOnTheContract component

---
+++ frontend/components/MainPage/sections/AlertSections/NoAvailableSlotsOnTheContract.tsx
@@ -0,0 +1,41 @@
+import { Flex, Typography } from 'antd';
+
+import { Pages } from '@/enums/PageState';
+import { usePageState } from '@/hooks/usePageState';
+import { useStakingContractInfo } from '@/hooks/useStakingContractInfo';
+
+import { CustomAlert } from '../../../Alert';
+
+const { Text } = Typography;
+
+export const NoAvailableSlotsOnTheContract = () => {
+  const { goto } = usePageState();
+  const { hasEnoughServiceSlots } = useStakingContractInfo();
+
+  if (hasEnoughServiceSlots) return null;
+
+  return (
+    <CustomAlert
+      type="warning"
+      fullWidth
+      showIcon
+      message={
+        <Flex justify="space-between" gap={4} vertical>
+          <Text className="font-weight-600">
+            No available slots on the contract
+          </Text>
+          <span className="text-sm">
+            Select a contract with available slots to be able to start your
+            agent.
+          </span>
+          <Text
+            className="pointer hover-underline text-primary text-sm"
+            onClick={() => goto(Pages.ManageStaking)}
+          >
+            Change staking contract
+          </Text>
+        </Flex>
+      }
+    />
+  );
+};
