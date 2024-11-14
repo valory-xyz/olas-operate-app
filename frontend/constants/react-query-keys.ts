ef679bddfc96749300a4f45f0d53034bed336983
mohandast52 <mohandast52@gmail.com>
2024-11-14 14:29:47 +0530

    refactor: enhance RewardProvider to utilize react-query for rewards data fetching

--- frontend/constants/react-query-keys.ts
+++ frontend/constants/react-query-keys.ts
@@ -2,4 +2,10 @@ export const REACT_QUERY_KEYS = {
   SERVICES_KEY: ['services'] as const,
   SERVICE_DEPLOYMENT_STATUS_KEY: (serviceConfigId: string) =>
     ['serviceStatus', serviceConfigId] as const,
+  REWARDS_KEY: (stakingProgramId: string, multisig: string, token: number) =>
+    ['rewards', stakingProgramId, multisig, token] as const,
+  AVAILABLE_REWARDS_FOR_EPOCH_KEY: (
+    stakingProgramId: string,
+    chainId: number,
+  ) => ['availableRewardsForEpoch', stakingProgramId, chainId] as const,
 } as const;
