c130efd926ee297f3a28f890ae2841822b50be8f
mohandast52 <mohandast52@gmail.com>
2024-11-15 17:34:43 +0530

    refactor: update StakingProgramProvider to use react-query for fetching active staking program

--- frontend/context/StakingProgramProvider.tsx
+++ frontend/context/StakingProgramProvider.tsx
@@ -1,75 +1,81 @@
-/*
-  This context provider is responsible for determining the current active staking program, if any.
-  It does so by checking if the current service is staked, and if so, which staking program it is staked in.
-  It also provides a method to update the active staking program id in state.
-*/
+/**
+ * This context provider is responsible for determining the current active staking program, if any.
+ * It does so by checking if the current service is staked, and if so, which staking program it is staked in.
+ * It also provides a method to update the active staking program id in state.
+ */
 
-import { createContext, PropsWithChildren, useCallback, useState } from 'react';
-import { useInterval } from 'usehooks-ts';
+import { useQuery, useQueryClient } from '@tanstack/react-query';
+import { createContext, PropsWithChildren, useCallback } from 'react';
 
-import { CHAIN_CONFIG } from '@/config/chains';
+import { AGENT_CONFIG } from '@/config/agents';
+import { GNOSIS_CHAIN_CONFIG } from '@/config/chains';
+import { FIVE_SECONDS_INTERVAL } from '@/constants/intervals';
+import { REACT_QUERY_KEYS } from '@/constants/react-query-keys';
 import { StakingProgramId } from '@/enums/StakingProgram';
+import { useService } from '@/hooks/useService';
 import { useServices } from '@/hooks/useServices';
-import { AutonolasService } from '@/service/Autonolas';
 
 export const INITIAL_DEFAULT_STAKING_PROGRAM_ID = StakingProgramId.Beta;
 
 export const StakingProgramContext = createContext<{
   activeStakingProgramId?: StakingProgramId | null;
   defaultStakingProgramId: StakingProgramId;
-  updateActiveStakingProgramId: () => Promise<void>;
-  setDefaultStakingProgramId: (stakingProgramId: StakingProgramId) => void;
 }>({
   activeStakingProgramId: undefined,
   defaultStakingProgramId: INITIAL_DEFAULT_STAKING_PROGRAM_ID,
-  updateActiveStakingProgramId: async () => {},
-  setDefaultStakingProgramId: () => {},
 });
+
+const currentAgent = AGENT_CONFIG.trader; // TODO: replace with dynamic agent selection
+const currentChainId = GNOSIS_CHAIN_CONFIG.chainId; // TODO: replace with dynamic chain selection
+
+const useGetActiveStakingProgramId = () => {
+  const queryClient = useQueryClient();
+
+  const { selectedService, isFetched: isLoaded } = useServices();
+  const serviceConfigId =
+    isLoaded && selectedService ? selectedService?.service_config_id : '';
+  const { service } = useService({ serviceConfigId });
+
+  // if no service nft, not staked
+  const serviceId = service?.chain_configs[currentChainId].chain_data?.token;
+
+  const response = useQuery({
+    queryKey: [REACT_QUERY_KEYS.STAKING_PROGRAM_KEY, serviceId, currentChainId],
+    queryFn: async () => {
+      return currentAgent.serviceApi.getCurrentStakingProgramByServiceId(
+        serviceId!,
+        currentChainId,
+      );
+    },
+    enabled: !!service,
+    refetchInterval: isLoaded ? FIVE_SECONDS_INTERVAL : false,
+  });
+
+  const setActiveStakingProgramId = useCallback(
+    (stakingProgramId: StakingProgramId | null) => {
+      queryClient.setQueryData(
+        [REACT_QUERY_KEYS.STAKING_PROGRAM_KEY, serviceId, currentChainId],
+        stakingProgramId,
+      );
+    },
+    [queryClient, serviceId],
+  );
+
+  return { ...response, setActiveStakingProgramId };
+};
 
 /**
  * Determines the current active staking program, if any
  * */
 export const StakingProgramProvider = ({ children }: PropsWithChildren) => {
-  const { service } = useServices();
-
-  const [activeStakingProgramId, setActiveStakingProgramId] =
-    useState<StakingProgramId | null>();
-
-  const [defaultStakingProgramId, setDefaultStakingProgramId] = useState(
-    INITIAL_DEFAULT_STAKING_PROGRAM_ID,
-  );
-
-  const updateActiveStakingProgramId = useCallback(async () => {
-    // if no service nft, not staked
-    const serviceId =
-      service?.chain_configs[CHAIN_CONFIG.OPTIMISM.chainId].chain_data?.token;
-
-    if (
-      !service?.chain_configs[CHAIN_CONFIG.OPTIMISM.chainId].chain_data?.token
-    ) {
-      setActiveStakingProgramId(null);
-      return;
-    }
-
-    if (serviceId) {
-      // if service exists, we need to check if it is staked
-      AutonolasService.getCurrentStakingProgramByServiceId(serviceId).then(
-        (stakingProgramId) => {
-          setActiveStakingProgramId(stakingProgramId);
-        },
-      );
-    }
-  }, [service]);
-
-  useInterval(updateActiveStakingProgramId, 5000);
+  const { data: activeStakingProgramId } = useGetActiveStakingProgramId();
 
   return (
     <StakingProgramContext.Provider
       value={{
         activeStakingProgramId,
-        updateActiveStakingProgramId,
-        defaultStakingProgramId,
-        setDefaultStakingProgramId,
+        // TODO: we should not expose the default staking program id, discuss with Josh
+        defaultStakingProgramId: INITIAL_DEFAULT_STAKING_PROGRAM_ID,
       }}
     >
       {children}
