68c420dd432be5590e7c544cc259afd30dc083ab
mohandast52 <mohandast52@gmail.com>
2024-12-02 14:42:12 +0530

    feat: refactor SelectYourAgent component to improve agent selection handling

--- frontend/components/SetupPage/SelectYourAgent.tsx
+++ frontend/components/SetupPage/SelectYourAgent.tsx
@@ -1,71 +1,88 @@
 import { Button, Card, Flex, Typography } from 'antd';
 import { entries } from 'lodash';
 import Image from 'next/image';
+import { useCallback } from 'react';
 
 import { AGENT_CONFIG } from '@/config/agents';
 import { COLOR } from '@/constants/colors';
 import { AgentType } from '@/enums/Agent';
+import { Pages } from '@/enums/Pages';
 import { SetupScreen } from '@/enums/SetupScreen';
+import { usePageState } from '@/hooks/usePageState';
 import { useServices } from '@/hooks/useServices';
+import { AgentConfig } from '@/types/Agent';
 
 import { CardFlex } from '../styled/CardFlex';
 import { SetupCreateHeader } from './Create/SetupCreateHeader';
 
 const { Title, Text } = Typography;
 
-export const SelectYourAgent = () => {
+type EachAgentProps = { agentType: AgentType; agentConfig: AgentConfig };
+const EachAgent = ({ agentType, agentConfig }: EachAgentProps) => {
   const { selectedAgentType, updateAgentType } = useServices();
+  const { goto } = usePageState();
+
+  const isCurrentAgent = selectedAgentType === agentType;
+
+  const handleSelectAgent = useCallback(() => {
+    updateAgentType(agentType);
+    goto(Pages.SetupYourAgent); // TODO: page to be added
+  }, [agentType, updateAgentType, goto]);
 
   return (
-    <CardFlex gap={10} styles={{ body: { padding: '12px 24px' } }}>
-      <SetupCreateHeader prev={SetupScreen.SetupBackupSigner} />
-      <Title level={3}>Select your agent</Title>
+    <Card
+      key={agentType}
+      style={{ padding: 0, marginBottom: 6 }}
+      styles={{
+        body: {
+          padding: 16,
+          gap: 6,
+          borderRadius: 'inherit',
+          background: isCurrentAgent ? COLOR.GRAY_1 : 'transparent',
+          opacity: isCurrentAgent ? 0.75 : 1,
+        },
+      }}
+    >
+      <Flex vertical>
+        <Flex align="center" justify="space-between">
+          <Image
+            src={`/agent-${agentType}-icon.png`}
+            width={50}
+            height={50}
+            alt={agentConfig.displayName}
+          />
+          {isCurrentAgent ? (
+            <Text>Selected Agent</Text>
+          ) : (
+            <Button type="primary" onClick={handleSelectAgent}>
+              Select
+            </Button>
+          )}
+        </Flex>
+      </Flex>
 
-      {entries(AGENT_CONFIG).map(([agentType, agentConfig]) => {
-        const isCurrentAgent = selectedAgentType === agentType;
+      <Title level={5} className="m-0">
+        {agentConfig.displayName}
+      </Title>
 
-        return (
-          <Card
-            key={agentType}
-            style={{ padding: 0, marginBottom: 6 }}
-            styles={{
-              body: {
-                padding: 16,
-                gap: 6,
-                borderRadius: 'inherit',
-                background: isCurrentAgent ? COLOR.GRAY_1 : 'transparent',
-              },
-            }}
-          >
-            <Flex vertical>
-              <Flex align="center" justify="space-between">
-                <Image
-                  src={`/agent-${agentType}-icon.png`}
-                  width={50}
-                  height={50}
-                  alt="Agent 1"
-                />
-                {isCurrentAgent ? (
-                  <Text>Selected Agent</Text>
-                ) : (
-                  <Button
-                    type="primary"
-                    onClick={() => updateAgentType(agentType as AgentType)}
-                  >
-                    Select
-                  </Button>
-                )}
-              </Flex>
-            </Flex>
-
-            <Title level={5} className="m-0">
-              {agentConfig.displayName}
-            </Title>
-
-            <Text type="secondary">{agentConfig.description}</Text>
-          </Card>
-        );
-      })}
-    </CardFlex>
+      <Text type="secondary">{agentConfig.description}</Text>
+    </Card>
   );
 };
+
+export const SelectYourAgent = () => (
+  <CardFlex gap={10} styles={{ body: { padding: '12px 24px' } }}>
+    <SetupCreateHeader prev={SetupScreen.SetupBackupSigner} />
+    <Title level={3}>Select your agent</Title>
+
+    {entries(AGENT_CONFIG).map(([agentType, agentConfig]) => {
+      return (
+        <EachAgent
+          key={agentType}
+          agentType={agentType as AgentType}
+          agentConfig={agentConfig}
+        />
+      );
+    })}
+  </CardFlex>
+);
