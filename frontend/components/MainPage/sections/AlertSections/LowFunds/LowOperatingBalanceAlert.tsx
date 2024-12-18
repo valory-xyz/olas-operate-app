ce2b8fff3f4fb373e7011063dcf8dde6aad9354c
mohandast52 <mohandast52@gmail.com>
2024-12-18 13:28:28 +0530

    refactor: enhance balance check logic in LowOperatingBalanceAlert component

--- frontend/components/MainPage/sections/AlertSections/LowFunds/LowOperatingBalanceAlert.tsx
+++ frontend/components/MainPage/sections/AlertSections/LowFunds/LowOperatingBalanceAlert.tsx
@@ -3,7 +3,10 @@ import { useMemo } from 'react';
 
 import { CustomAlert } from '@/components/Alert';
 import { WalletType } from '@/enums/Wallet';
-import { useMasterBalances } from '@/hooks/useBalanceContext';
+import {
+  useMasterBalances,
+  useServiceBalances,
+} from '@/hooks/useBalanceContext';
 import { useServices } from '@/hooks/useServices';
 import { useStore } from '@/hooks/useStore';
 
@@ -17,27 +20,43 @@ const { Text, Title } = Typography;
  */
 export const LowOperatingBalanceAlert = () => {
   const { storeState } = useStore();
-  const { selectedAgentType } = useServices();
+  const { selectedAgentType, selectedService } = useServices();
   const { isLoaded: isBalanceLoaded, masterSafeNativeGasBalance } =
     useMasterBalances();
+  const { serviceSafeBalances } = useServiceBalances(
+    selectedService?.service_config_id,
+  );
 
   const { chainName, tokenSymbol, masterSafeAddress, masterThresholds } =
     useLowFundsDetails();
 
-  const isLowBalance = useMemo(() => {
-    if (!masterSafeNativeGasBalance) return false;
-    if (!masterThresholds) return false;
+  const hasEnoughBalance = useMemo(() => {
+    if (!masterSafeNativeGasBalance) return true;
+    if (!serviceSafeBalances) return true;
+    if (!masterThresholds) return true;
 
+    const safeThreshold = masterThresholds[WalletType.Safe][tokenSymbol];
+    const agentSafeNativeBalance =
+      serviceSafeBalances.find(({ symbol }) => symbol === tokenSymbol)
+        ?.balance || 0;
+
+    // @note: Funds are transferred to the agent safe from the master safe.
+    // Hence, if the agent safe has enough funds, it is considered as enough.
     return (
-      masterSafeNativeGasBalance <
-      masterThresholds[WalletType.Safe][tokenSymbol]
+      masterSafeNativeGasBalance >= safeThreshold ||
+      agentSafeNativeBalance >= safeThreshold
     );
-  }, [masterSafeNativeGasBalance, masterThresholds, tokenSymbol]);
+  }, [
+    masterSafeNativeGasBalance,
+    masterThresholds,
+    tokenSymbol,
+    serviceSafeBalances,
+  ]);
 
   if (!isBalanceLoaded) return null;
   if (!masterThresholds) return null;
   if (!storeState?.[`isInitialFunded_${selectedAgentType}`]) return;
-  if (!isLowBalance) return null;
+  if (hasEnoughBalance) return null;
 
   return (
     <CustomAlert
