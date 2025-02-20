83edb9487ee69b6c120cd8dbb4c712c59ae267ee
truemiller <mlxndrj@gmail.com>
2025-02-20 15:35:29 +0000

    feat: update hash for Modius service template

--- frontend/constants/serviceTemplates.ts
+++ frontend/constants/serviceTemplates.ts
@@ -240,7 +240,7 @@ export const AGENTS_FUN_CELO_TEMPLATE: ServiceTemplate = {
 export const MODIUS_SERVICE_TEMPLATE: ServiceTemplate = {
   agentType: AgentType.Modius,
   name: 'Optimus', // Should be unique across all services and not be updated
-  hash: 'bafybeia4oqdiabgft3r2juyxn2bxltdpnmbhhosxli2ihcpfpffew52bmu',
+  hash: 'bafybeidsmy2rmftexz5ofbsu64vatakgraaxvxys7m5a5wkt7k3ipo46la',
   description: 'Optimus',
   image:
     'https://gateway.autonolas.tech/ipfs/bafybeiaakdeconw7j5z76fgghfdjmsr6tzejotxcwnvmp3nroaw3glgyve',
