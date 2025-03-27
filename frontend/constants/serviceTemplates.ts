0f207965384969fd6404643be2251847de5a7f43
mohandast52 <mohandast52@gmail.com>
2025-03-28 00:38:45 +0530

    feat: update hash for Optimus service template in serviceTemplates and optimus.yaml

--- frontend/constants/serviceTemplates.ts
+++ frontend/constants/serviceTemplates.ts
@@ -253,7 +253,7 @@ export const AGENTS_FUN_CELO_TEMPLATE: ServiceTemplate = {
 export const MODIUS_SERVICE_TEMPLATE: ServiceTemplate = {
   agentType: AgentType.Modius,
   name: 'Optimus', // Should be unique across all services and not be updated
-  hash: 'bafybeic4heytta52ot3532gan7kdpyyxjfbdtnifasa3lonnksclyhvc6m',
+  hash: 'bafybeiarzdiqj3bqddmop4jhphcvw4ufemq24xb6qa4jnz4bhtbd23a4r4',
   description: 'Optimus',
   image:
     'https://gateway.autonolas.tech/ipfs/bafybeiaakdeconw7j5z76fgghfdjmsr6tzejotxcwnvmp3nroaw3glgyve',
