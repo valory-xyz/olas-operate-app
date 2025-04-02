ef1f088db41c9018a87b69c015a25a393773b44f
Atatakai <densatsu@mail.ru>
2025-04-02 19:39:50 +0400

    feat: add deposit for bridging component

--- frontend/components/SetupPage/Create/BridgeOnEvm.tsx
+++ frontend/components/SetupPage/Create/BridgeOnEvm.tsx
@@ -1,6 +1,7 @@
 import { Button, Typography } from 'antd';
 import { useCallback } from 'react';
 
+import { DepositForBridging } from '@/components/bridge/DepositForBridging';
 import { CardFlex } from '@/components/styled/CardFlex';
 import { CardSection } from '@/components/styled/CardSection';
 import { SetupScreen } from '@/enums/SetupScreen';
@@ -9,6 +10,8 @@ import { useSetup } from '@/hooks/useSetup';
 import { SetupCreateHeader } from './SetupCreateHeader';
 
 const { Text, Title } = Typography;
+
+const FROM_CHAIN_NAME = 'Ethereum';
 
 // TODO: Tanya to update the component
 export const BridgeOnEvm = () => {
@@ -25,12 +28,13 @@ export const BridgeOnEvm = () => {
 
       <CardSection vertical gap={16} className="m-0 pt-24">
         <Title level={3} className="m-0">
-          Bridge from Ethereum
+          Bridge from {FROM_CHAIN_NAME}
         </Title>
         <Text className="text-base">
           The bridged amount covers all funds required to create your account
           and run your agent, including fees. No further funds will be needed.
         </Text>
+        <DepositForBridging chainName={FROM_CHAIN_NAME} />
         <Button onClick={handleMove} block type="primary" size="large">
           {'TODO => next'}
         </Button>
---
+++ frontend/components/bridge/DepositForBridging.tsx
@@ -0,0 +1,170 @@
+import {
+  CheckSquareOutlined,
+  ClockCircleOutlined,
+  CopyOutlined,
+  LoadingOutlined,
+} from '@ant-design/icons';
+import {
+  Button,
+  Divider,
+  Flex,
+  message,
+  Spin,
+  Tooltip,
+  Typography,
+} from 'antd';
+import { formatEther } from 'ethers/lib/utils';
+import { kebabCase } from 'lodash';
+import Image from 'next/image';
+import { useCallback, useState } from 'react';
+import styled from 'styled-components';
+
+import { AddressZero } from '@/constants/address';
+import { COLOR } from '@/constants/colors';
+import { TokenSymbol } from '@/enums/Token';
+import { copyToClipboard } from '@/utils/copyToClipboard';
+
+const { Text } = Typography;
+
+const RootCard = styled.div`
+  display: flex;
+  flex-direction: column;
+  border-radius: 12px;
+  align-items: flex-start;
+  border: 1px solid ${COLOR.BORDER_GRAY};
+`;
+
+// TODO: move to some shared place? e.g. constants?
+const LIGHT_ICON_STYLE = { color: COLOR.TEXT_LIGHT };
+const WARNING_ICON_STYLE = { color: COLOR.WARNING };
+const SUCCESS_ICON_STYLE = { color: COLOR.SUCCESS };
+
+type TokenInfoProps = {
+  status: 'waiting' | 'received';
+  symbol: TokenSymbol;
+  totalRequiredInWei: bigint;
+  currentBalanceInWei: bigint;
+};
+
+const TokenInfo = ({
+  status,
+  symbol,
+  totalRequiredInWei,
+  currentBalanceInWei,
+}: TokenInfoProps) => {
+  if (status === 'waiting') {
+    return (
+      <Flex gap={8}>
+        <ClockCircleOutlined style={WARNING_ICON_STYLE} />
+        <Text className="loading-ellipses">
+          Waiting for{' '}
+          <Text className="font-weight-600">
+            {formatEther(totalRequiredInWei - currentBalanceInWei)}&nbsp;
+            {symbol}
+          </Text>
+        </Text>
+        {/** TODO: add tooltip */}
+      </Flex>
+    );
+  }
+
+  if (status === 'received') {
+    return (
+      <Flex gap={8}>
+        <CheckSquareOutlined style={SUCCESS_ICON_STYLE} />
+        <Text className="font-weight-600">{symbol}</Text> funds received!
+        {/** TODO: add tooltip */}
+      </Flex>
+    );
+  }
+  return null;
+};
+
+type DepositForBridgingProps = {
+  chainName: string;
+};
+export const DepositForBridging = ({ chainName }: DepositForBridgingProps) => {
+  // TODO: use API for getting quote
+  const [isRequestingQuote, setIsRequestingQuote] = useState(true);
+  const [depositAddress, setDepositAddress] = useState(AddressZero);
+  const [tokens, setTokens] = useState<TokenInfoProps[]>([
+    {
+      status: 'waiting',
+      symbol: TokenSymbol.OLAS,
+      totalRequiredInWei: BigInt('40000000000000000000'), // 40 Ether
+      currentBalanceInWei: BigInt(0),
+    },
+    {
+      status: 'received',
+      symbol: TokenSymbol.ETH,
+      totalRequiredInWei: BigInt('55000000000000000'), // 0.055 Ether
+      currentBalanceInWei: BigInt('55000000000000000'), // 0.055 Ether
+    },
+  ]);
+
+  const handleCopyAddress = useCallback(() => {
+    if (depositAddress) {
+      copyToClipboard(depositAddress).then(() =>
+        message.success('Address copied!'),
+      );
+    }
+  }, [depositAddress]);
+
+  return (
+    <RootCard>
+      <Flex gap={8} align="center" className="p-16">
+        <Image
+          src={`/chains/${kebabCase(chainName)}-chain.png`}
+          width={20}
+          height={20}
+          alt="chain logo"
+        />
+        <Text>{chainName}</Text>
+      </Flex>
+
+      <Divider className="m-0" />
+
+      {isRequestingQuote ? (
+        <Flex gap={8} className="p-16">
+          <Spin indicator={<LoadingOutlined spin style={LIGHT_ICON_STYLE} />} />
+          <Text className="loading-ellipses">Requesting quote</Text>
+        </Flex>
+      ) : (
+        <>
+          <Flex gap={8} align="start" vertical className="p-16">
+            {tokens.map((token) => (
+              <TokenInfo
+                key={token.symbol}
+                status={token.status}
+                symbol={token.symbol}
+                totalRequiredInWei={token.totalRequiredInWei}
+                currentBalanceInWei={token.currentBalanceInWei}
+              />
+            ))}
+          </Flex>
+
+          <Divider className="m-0" />
+
+          <Flex gap={8} vertical className="p-16">
+            <Flex justify="space-between">
+              <Text className="text-sm" type="secondary">
+                Deposit address
+              </Text>
+              <Flex gap={10} align="center">
+                <Tooltip title="Copy to clipboard" placement="left">
+                  <Button
+                    onClick={handleCopyAddress}
+                    size="small"
+                    icon={<CopyOutlined style={LIGHT_ICON_STYLE} />}
+                  />
+                </Tooltip>
+              </Flex>
+            </Flex>
+
+            <span className="can-select-text break-word">{`${depositAddress}`}</span>
+          </Flex>
+        </>
+      )}
+    </RootCard>
+  );
+};
