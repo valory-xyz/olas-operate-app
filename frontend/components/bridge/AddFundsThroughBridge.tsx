c0d8926987f86030c59359cbb863b62513173750
mohandast52 <mohandast52@gmail.com>
2025-04-15 17:03:24 +0530

    feat: refactor AddFundsThroughBridge to use Form for token amount input and improve component structure

--- frontend/components/bridge/AddFundsThroughBridge.tsx
+++ frontend/components/bridge/AddFundsThroughBridge.tsx
@@ -1,5 +1,6 @@
 import { ArrowLeftOutlined } from '@ant-design/icons';
-import { Button, Flex, Image, Typography } from 'antd';
+import { Button, Flex, Form, Image, Typography } from 'antd';
+import { useMemo } from 'react';
 
 import { Pages } from '@/enums/Pages';
 import { TokenSymbol } from '@/enums/Token';
@@ -11,6 +12,7 @@ import { CardFlex } from '../styled/CardFlex';
 
 const { Title, Text } = Typography;
 
+// TODO: from backend
 const fundsToReceive = [
   {
     symbol: TokenSymbol.OLAS,
@@ -22,6 +24,46 @@ const fundsToReceive = [
   },
 ];
 
+const BridgeHeader = () => {
+  const { goto } = usePageState();
+
+  return (
+    <Flex gap={16} align="center">
+      <Button onClick={() => goto(Pages.Main)} icon={<ArrowLeftOutlined />} />
+      <Title level={5} className="m-0">
+        Bridge from Ethereum
+      </Title>
+    </Flex>
+  );
+};
+
+const InputAddOn = ({ symbol }: { symbol: TokenSymbol }) => {
+  const imgSrc = useMemo(() => {
+    if (!symbol) return;
+    if (symbol === TokenSymbol.OLAS) {
+      return '/olas-icon.png';
+    }
+    return `/chains/${toMiddlewareChainFromTokenSymbol(symbol)}-chain.png`;
+  }, [symbol]);
+
+  return (
+    <Flex align="center" justify="flex-start" gap={6} style={{ width: 78 }}>
+      {imgSrc && (
+        <>
+          <Image
+            src={imgSrc}
+            alt={symbol}
+            style={{ width: 20, height: 20 }}
+            preview={false}
+          />{' '}
+        </>
+      )}
+
+      {symbol}
+    </Flex>
+  );
+};
+
 export const AddFundsThroughBridge = () => {
   const { goto } = usePageState();
 
@@ -29,80 +71,63 @@ export const AddFundsThroughBridge = () => {
     <CardFlex
       gap={20}
       noBorder
-      styles={{
-        header: { minHeight: 56 },
-        body: { padding: '0 24px' },
-      }}
-      title={
-        <Flex gap={16} align="center">
-          <Button
-            onClick={() => goto(Pages.Main)}
-            icon={<ArrowLeftOutlined />}
-          />
-          <Title level={5} className="m-0">
-            Bridge from Ethereum
-          </Title>
-        </Flex>
-      }
+      title={<BridgeHeader />}
+      styles={{ header: { minHeight: 56 }, body: { padding: '0 24px' } }}
     >
       <Text>
         Specify the amount of tokens you&apos;d like to receive to your Pearl
         Safe.
       </Text>
 
-      <Flex gap={12} vertical>
+      <Flex gap={8} vertical>
         <Text className="font-xs" type="secondary">
           Amount to receive
         </Text>
-        {fundsToReceive.map(({ symbol, amount }) => {
-          const imgSrc = (() => {
-            if (!symbol) return;
-            if (symbol === TokenSymbol.OLAS) {
-              return '/olas-icon.png';
-            }
-            return `/chains/${toMiddlewareChainFromTokenSymbol(symbol)}-chain.png`;
-          })();
-
-          return (
-            <NumberInput
-              key={symbol}
-              value={amount}
-              addonBefore={
-                <Flex
-                  align="center"
-                  justify="flex-start"
-                  gap={6}
-                  style={{ width: 78 }}
+        <Flex gap={12} vertical>
+          <Form
+            layout="vertical"
+            onFinish={(values) => {
+              console.log('Form Values:', values);
+            }}
+          >
+            {fundsToReceive.map(({ symbol, amount }) => {
+              return (
+                <Form.Item
+                  key={symbol}
+                  name={symbol}
+                  label={null}
+                  initialValue={amount}
+                  rules={[
+                    {
+                      required: true,
+                      message: '',
+                    },
+                    {
+                      type: 'number',
+                      min: 0,
+                      message: '',
+                    },
+                  ]}
+                  validateTrigger="onSubmit"
                 >
-                  {imgSrc && (
-                    <>
-                      <Image
-                        src={imgSrc}
-                        alt={symbol}
-                        style={{ width: 20, height: 20 }}
-                        preview={false}
-                      />{' '}
-                    </>
-                  )}
+                  <NumberInput
+                    addonBefore={<InputAddOn symbol={symbol} />}
+                    placeholder="0.00"
+                    size="large"
+                    min={0}
+                    className="w-full"
+                  />
+                </Form.Item>
+              );
+            })}
 
-                  {symbol}
-                </Flex>
-              }
-              placeholder="0.00"
-              size="large"
-              min={0}
-            />
-          );
-        })}
-
-        <Button
-          type="primary"
-          size="large"
-          onClick={() => goto(Pages.Main)}
-          disabled // TODO: Mohan to update
-        >
-          Bridge funds
-        </Button>
+            <Form.Item>
+              <Button type="primary" size="large" htmlType="submit" block>
+                Bridge funds
+              </Button>
+            </Form.Item>
+          </Form>
+        </Flex>
       </Flex>
     </CardFlex>
   );
