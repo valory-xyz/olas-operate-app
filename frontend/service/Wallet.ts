07202e3dd73892586fbee83faeb2c9071019c007
mohandast52 <mohandast52@gmail.com>
2025-04-17 04:42:12 +0530

    refactor: remove mock implementation from createSafe function

--- frontend/service/Wallet.ts
+++ frontend/service/Wallet.ts
@@ -4,13 +4,6 @@ import { BACKEND_URL } from '@/constants/urls';
 import { Address } from '@/types/Address';
 import { SafeCreationResponse } from '@/types/Wallet';
 
-const IS_MOCK_ENABLED = true;
-
-const createSafeMock = {
-  safe: '0x29e23F7705d849F368855947691cB133CD770752',
-  message: 'Safe created!',
-} as const satisfies SafeCreationResponse;
-
 /**
  * Returns a list of available wallets
  */
@@ -41,16 +34,14 @@ const createSafe = async (
   backup_owner?: string,
   initial_funds?: { [address: Address]: bigint | number },
 ): Promise<SafeCreationResponse> =>
-  IS_MOCK_ENABLED
-    ? Promise.resolve(createSafeMock)
-    : fetch(`${BACKEND_URL}/wallet/safe`, {
-        method: 'POST',
-        headers: { ...CONTENT_TYPE_JSON_UTF8 },
-        body: JSON.stringify({ chain, backup_owner, initial_funds }),
-      }).then((res) => {
-        if (res.ok) return res.json();
-        throw new Error('Failed to create safe');
-      });
+  fetch(`${BACKEND_URL}/wallet/safe`, {
+    method: 'POST',
+    headers: { ...CONTENT_TYPE_JSON_UTF8 },
+    body: JSON.stringify({ chain, backup_owner, initial_funds }),
+  }).then((res) => {
+    if (res.ok) return res.json();
+    throw new Error('Failed to create safe');
+  });
 
 const updateSafeBackupOwner = async (
   chain: MiddlewareChain,
