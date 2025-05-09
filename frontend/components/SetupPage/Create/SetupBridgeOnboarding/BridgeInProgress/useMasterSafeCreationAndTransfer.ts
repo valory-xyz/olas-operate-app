fed179a7a98565f4be18b8adc904f5e08706f388
Mohan <mohandast52@gmail.com>
2025-05-09 15:58:57 +0530

    feat: use "transfer_excess_assets" instead of initial funds (#890)

--- frontend/components/SetupPage/Create/SetupBridgeOnboarding/BridgeInProgress/useMasterSafeCreationAndTransfer.ts
+++ frontend/components/SetupPage/Create/SetupBridgeOnboarding/BridgeInProgress/useMasterSafeCreationAndTransfer.ts
@@ -1,16 +1,10 @@
 import { useMutation } from '@tanstack/react-query';
-import { useMemo } from 'react';
 
-import { AddressBalanceRecord } from '@/client';
 import { TokenSymbol } from '@/enums/Token';
 import { useBackupSigner } from '@/hooks/useBackupSigner';
-import { useBalanceAndRefillRequirementsContext } from '@/hooks/useBalanceAndRefillRequirementsContext';
 import { useServices } from '@/hooks/useServices';
-import { useMasterWalletContext } from '@/hooks/useWallet';
 import { WalletService } from '@/service/Wallet';
-import { Address } from '@/types/Address';
 import { BridgingStepStatus } from '@/types/Bridge';
-import { bigintMax } from '@/utils/calculations';
 
 /**
  * Hook to create master safe and transfer funds (step 2 and 3)
@@ -19,69 +13,17 @@ export const useMasterSafeCreationAndTransfer = (
   tokenSymbols: TokenSymbol[],
 ) => {
   const backupSignerAddress = useBackupSigner();
-  const { masterEoa } = useMasterWalletContext();
-  const {
-    isBalancesAndFundingRequirementsLoading,
-    balances,
-    totalRequirements,
-  } = useBalanceAndRefillRequirementsContext();
   const { selectedAgentConfig } = useServices();
 
   const chain = selectedAgentConfig.middlewareHomeChainId;
 
-  const initialFunds = useMemo(() => {
-    if (isBalancesAndFundingRequirementsLoading) return;
-    if (!balances) return;
-    if (!masterEoa) return;
-
-    return Object.entries(balances[masterEoa.address]).reduce(
-      (acc, [tokenAddress, tokenBalance]) => {
-        /**
-         * @example
-         * {
-         *   [0xMasterEoaAddress]: { 0x00000000...: amount, 0x00000001...: amount }
-         * }
-         * */
-        const totalAmountRequiredByMasterEoaToken =
-          (totalRequirements as AddressBalanceRecord)?.[masterEoa.address]?.[
-            tokenAddress as Address
-          ] || 0;
-
-        /**
-         * @note Need to keep some funds in the EOA for gas, and transfer the rest to the master safe.
-         * Amount to transfer to master safe = (amount available in EOA - amount required by master EOA)
-         *
-         * @example
-         * - EOA has "10 ETH" (User's balance)
-         * - Master EOA requires just "2 ETH"
-         * - Amount to transfer to master safe = 10 - 2 = "8 ETH"
-         */
-        const amountToTransferToMasterSafe = bigintMax(
-          BigInt(tokenBalance) - BigInt(totalAmountRequiredByMasterEoaToken),
-          BigInt(0),
-        );
-        acc[tokenAddress as Address] = amountToTransferToMasterSafe.toString();
-
-        return acc;
-      },
-      {} as Record<Address, string>,
-    );
-  }, [
-    isBalancesAndFundingRequirementsLoading,
-    masterEoa,
-    balances,
-    totalRequirements,
-  ]);
-
   return useMutation({
     mutationFn: async () => {
-      if (!initialFunds) return;
-
       try {
         const response = await WalletService.createSafe(
           chain,
           backupSignerAddress,
-          initialFunds,
+          true, // transfer excess assets ("true" for bridge onboarding)
         );
 
         return {
