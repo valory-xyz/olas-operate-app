9903c9edff3ef0edeb5427e11a31c351d2bf87c1
mohandast52 <mohandast52@gmail.com>
2025-06-11 12:10:08 +0530

    fix: update hash for Modius service template

--- frontend/constants/serviceTemplates.ts
+++ frontend/constants/serviceTemplates.ts
@@ -271,7 +271,7 @@ export const AGENTS_FUN_CELO_TEMPLATE: ServiceTemplate = {
 export const MODIUS_SERVICE_TEMPLATE: ServiceTemplate = {
   agentType: AgentType.Modius,
   name: 'Optimus',
-  hash: 'bafybeih7ylrn6fe7hqeu7hquexczta46v72hlvwivos67fgxgemkwsbvha',
+  hash: 'bafybeibbny4zyyjogw5mcw5ol5te57ljlfuqghw43m3ghhcv5fk57p2ugm',
   description: `${KPI_DESC_PREFIX} Optimus`,
   image:
     'https://gateway.autonolas.tech/ipfs/bafybeiaakdeconw7j5z76fgghfdjmsr6tzejotxcwnvmp3nroaw3glgyve',
@@ -422,7 +422,7 @@ export const MODIUS_SERVICE_TEMPLATE: ServiceTemplate = {
 export const OPTIMUS_SERVICE_TEMPLATE: ServiceTemplate = {
   agentType: AgentType.Optimus,
   name: 'Optimus - Optimism',
-  hash: 'bafybeih7ylrn6fe7hqeu7hquexczta46v72hlvwivos67fgxgemkwsbvha',
+  hash: 'bafybeibbny4zyyjogw5mcw5ol5te57ljlfuqghw43m3ghhcv5fk57p2ugm',
   description: `${KPI_DESC_PREFIX} Optimus service deployment on Optimism network`,
   image:
     'https://gateway.autonolas.tech/ipfs/bafybeiaakdeconw7j5z76fgghfdjmsr6tzejotxcwnvmp3nroaw3glgyve',
