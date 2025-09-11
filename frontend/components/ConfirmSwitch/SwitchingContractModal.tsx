5d5ad1af6e096701ed961b9777c634c0b4dd336e
Rajat Verma <rajatverma5885045@gmail.com>
2025-09-11 18:02:18 +0530

    fix: readable contract name

--- frontend/components/ConfirmSwitch/SwitchingContractModal.tsx
+++ frontend/components/ConfirmSwitch/SwitchingContractModal.tsx
@@ -4,6 +4,7 @@ import { Button, Spin } from 'antd';
 import { SuccessTickSvg } from '@/components/custom-icons/SuccessTick';
 import { Modal } from '@/components/ui/Modal';
 import { useYourWallet } from '@/components/YourWalletPage/useYourWallet';
+import { STAKING_PROGRAMS } from '@/config/stakingPrograms';
 import { AddressZero } from '@/constants/address';
 import { Pages } from '@/enums/Pages';
 import { usePageState } from '@/hooks/usePageState';
@@ -34,13 +35,17 @@ export const SwitchingContractModal = ({
   const { selectedAgentConfig } = useServices();
   const { serviceSafe } = useYourWallet();
   const agentName = generateName(serviceSafe?.address ?? AddressZero);
+  const stakingProgramMeta =
+    STAKING_PROGRAMS[selectedAgentConfig.evmHomeChainId][
+      stakingProgramIdToMigrateTo
+    ];
 
   const title = isSwitchingContract
     ? 'Switching in Progress'
     : 'Contract Switched Successfully!';
   const description = isSwitchingContract
     ? 'Your agent is switching contracts. It usually takes up to 5 min. Please keep the app open until the process is complete.'
-    : `Your ${selectedAgentConfig.displayName} ${agentName} is now staked on ${stakingProgramIdToMigrateTo} staking contract.`;
+    : `Your ${selectedAgentConfig.displayName} ${agentName} is now staked on ${stakingProgramMeta?.name} staking contract.`;
 
   return (
     <Modal
