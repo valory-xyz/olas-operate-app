6a28681943bcd77f794611e3e50dff05eb2e2a18
mohandast52 <mohandast52@gmail.com>
2025-09-26 05:13:49 +0530

    feat: add TokenAmountInput for managing withdrawal amounts in FundAgent component

--- frontend/components/AgentWallet/FundAgent/FundAgent.tsx
+++ frontend/components/AgentWallet/FundAgent/FundAgent.tsx
@@ -1,12 +1,14 @@
 import { ArrowRightOutlined } from '@ant-design/icons';
-import { Flex, Image, Typography } from 'antd';
+import { Button, Flex, Image, Typography } from 'antd';
 
 import { BackButton } from '@/components/ui/BackButton';
 import { CardFlex } from '@/components/ui/CardFlex';
 import { Divider } from '@/components/ui/Divider';
+import { TokenAmountInput } from '@/components/ui/TokenAmountInput';
 
 import { useAgentWallet } from '../AgentWalletProvider';
 import { cardStyles } from '../common';
+import { useFundAgent } from './useFundAgent';
 
 const { Title, Text } = Typography;
 
@@ -27,18 +29,18 @@ const PearlWalletToExternalWallet = () => {
     <Flex vertical style={{ margin: '0 -32px' }}>
       <Divider className="m-0" />
       <Flex gap={16} style={{ padding: '12px 32px' }} align="center">
-        <Flex gap={8} align="center">
+        <Text>
           <Text type="secondary">From</Text>{' '}
+          <Text className="font-weight-500">Pearl Wallet</Text>
+        </Text>
+        <ArrowRightOutlined style={{ fontSize: 12 }} />
+        <Flex gap={8} align="center">
+          <Text type="secondary">To</Text>{' '}
           {agentName && agentImgSrc && (
             <Image src={agentImgSrc} width={28} height={28} alt={agentName} />
           )}
           <Text className="font-weight-500">{agentName}</Text>
         </Flex>
-        <ArrowRightOutlined style={{ fontSize: 12 }} />
-        <Text>
-          <Text type="secondary">To</Text>{' '}
-          <Text className="font-weight-500">Pearl Wallet</Text>
-        </Text>
       </Flex>
       <Divider className="m-0" />
     </Flex>
@@ -48,6 +50,8 @@ const PearlWalletToExternalWallet = () => {
 type FundAgentProps = { onBack: () => void };
 
 export const FundAgent = ({ onBack }: FundAgentProps) => {
+  const { availableAssets, amountsToWithdraw, onAmountChange } = useFundAgent();
+
   return (
     <Flex gap={16} vertical style={cardStyles}>
       <CardFlex $noBorder $padding="32px" className="w-full">
@@ -57,7 +61,26 @@ export const FundAgent = ({ onBack }: FundAgentProps) => {
             <FundAgentTitle />
           </Flex>
           <PearlWalletToExternalWallet />
+
+          <Flex justify="space-between" align="center" vertical gap={16}>
+            {availableAssets.map(({ amount, valueInUsd, symbol }) => (
+              <TokenAmountInput
+                key={symbol}
+                tokenSymbol={symbol}
+                value={amountsToWithdraw?.[symbol] ?? 0}
+                totalAmount={amount}
+                totalAmountInUsd={valueInUsd}
+                onChange={(x) => onAmountChange(symbol, x ?? 0)}
+              />
+            ))}
+          </Flex>
         </Flex>
+      </CardFlex>
+
+      <CardFlex $noBorder $padding="32px" className="w-full">
+        <Button type="primary" className="w-full" size="large">
+          Confirm Transfer
+        </Button>
       </CardFlex>
     </Flex>
   );
