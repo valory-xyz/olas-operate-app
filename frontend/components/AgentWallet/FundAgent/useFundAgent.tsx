9fcba59cfa775111e1e7e109da48fc6d7457de4f
mohandast52 <mohandast52@gmail.com>
2025-09-26 05:08:49 +0530

    feat: implement useFundAgent hook for managing withdrawal amounts and available assets

---
+++ frontend/components/AgentWallet/FundAgent/useFundAgent.tsx
@@ -0,0 +1,121 @@
+import { sum } from 'lodash';
+import { useCallback, useMemo, useState } from 'react';
+
+import { CHAIN_CONFIG } from '@/config/chains';
+import { TOKEN_CONFIG, TokenConfig } from '@/config/tokens';
+import { TokenSymbol, TokenSymbolMap } from '@/constants/token';
+import {
+  useBalanceContext,
+  useMasterBalances,
+  useRewardContext,
+  useService,
+  useServices,
+  useUsdAmounts,
+} from '@/hooks';
+import { asEvmChainDetails } from '@/utils/middlewareHelpers';
+
+import { AvailableAsset } from '../types';
+
+export const useFundAgent = () => {
+  const {
+    isLoading: isServicesLoading,
+    selectedAgentConfig,
+    selectedService,
+  } = useServices();
+  const { isLoaded } = useService(selectedService?.service_config_id);
+  const { isLoading: isBalanceLoading } = useBalanceContext();
+  const { accruedServiceStakingRewards } = useRewardContext();
+  const {
+    masterEoaBalance,
+    masterSafeNativeBalance,
+    masterSafeOlasBalance,
+    masterSafeErc20Balances,
+  } = useMasterBalances();
+
+  const { evmHomeChainId, middlewareHomeChainId } = selectedAgentConfig;
+
+  const [amountsToWithdraw, setAmountsToWithdraw] = useState<
+    Partial<Record<TokenSymbol, number>>
+  >({});
+
+  const chainName = evmHomeChainId
+    ? (CHAIN_CONFIG[evmHomeChainId].name as string)
+    : '';
+
+  const usdRequirements = useMemo(() => {
+    if (!evmHomeChainId) return [];
+    return Object.entries(TOKEN_CONFIG[evmHomeChainId!]).map(
+      ([untypedSymbol]) => {
+        const symbol = untypedSymbol as TokenSymbol;
+        return { symbol, amount: 0 };
+      },
+    );
+  }, [evmHomeChainId]);
+
+  const { breakdown: usdBreakdown } = useUsdAmounts(chainName, usdRequirements);
+
+  // OLAS token, Native Token, other ERC20 tokens
+  const availableAssets: AvailableAsset[] = useMemo(() => {
+    if (!evmHomeChainId) return [];
+
+    const tokenConfig = TOKEN_CONFIG[evmHomeChainId];
+    return Object.entries(tokenConfig).map(
+      ([untypedSymbol, untypedTokenDetails]) => {
+        const symbol = untypedSymbol as TokenSymbol;
+        const { address } = untypedTokenDetails as TokenConfig;
+        const { usdPrice } = usdBreakdown.find(
+          (breakdown) => breakdown.symbol === symbol,
+        ) ?? { usdPrice: 0 };
+
+        const balance = (() => {
+          // balance for OLAS
+          if (symbol === TokenSymbolMap.OLAS) {
+            return sum([masterSafeOlasBalance, accruedServiceStakingRewards]);
+          }
+
+          // balance for native tokens
+          if (symbol === asEvmChainDetails(middlewareHomeChainId).symbol) {
+            return sum([masterSafeNativeBalance, masterEoaBalance]);
+          }
+
+          // balance for other required tokens (eg. USDC)
+          return masterSafeErc20Balances?.[symbol] ?? 0;
+        })();
+
+        const asset: AvailableAsset = {
+          address,
+          symbol,
+          amount: balance,
+          valueInUsd: usdPrice * balance,
+        };
+        return asset;
+      },
+    );
+  }, [
+    evmHomeChainId,
+    usdBreakdown,
+    middlewareHomeChainId,
+    masterSafeErc20Balances,
+    masterSafeOlasBalance,
+    accruedServiceStakingRewards,
+    masterSafeNativeBalance,
+    masterEoaBalance,
+  ]);
+
+  const onAmountChange = useCallback((symbol: TokenSymbol, amount: number) => {
+    setAmountsToWithdraw((prev) => ({ ...prev, [symbol]: amount }));
+  }, []);
+
+  const onReset = useCallback(() => {
+    setAmountsToWithdraw({});
+  }, []);
+
+  return {
+    isLoading: isServicesLoading || !isLoaded || isBalanceLoading,
+    evmHomeChainId,
+    availableAssets,
+    amountsToWithdraw,
+    onAmountChange,
+    onReset,
+  };
+};
