41156736263dbe4b4ac09e7e7f9487b4dfbbc00f
Atatakai <densatsu@mail.ru>
2025-10-06 19:10:07 +0200

    fix: under construction alert

--- frontend/components/MainPageV1/Home/Overview/AgentInfo/AgentDisabledAlert/UnderConstructionAlert.tsx
+++ frontend/components/MainPageV1/Home/Overview/AgentInfo/AgentDisabledAlert/UnderConstructionAlert.tsx
@@ -2,20 +2,23 @@ import { Button, Typography } from 'antd';
 
 import { CustomAlert } from '@/components/Alert';
 import { Pages } from '@/enums/Pages';
+import { useServices } from '@/hooks';
 import { usePageState } from '@/hooks/usePageState';
 import { useSharedContext } from '@/hooks/useSharedContext';
 
 const { Text } = Typography;
 
 export const UnderConstructionAlert = () => {
+  const { selectedAgentConfig } = useServices();
   const { goto } = usePageState();
   const { mainOlasBalance } = useSharedContext();
 
-  const canWithdraw = mainOlasBalance === 0;
+  const hasExternalFunds = selectedAgentConfig.hasExternalFunds;
+  const canWithdraw = mainOlasBalance !== 0;
 
   return (
     <CustomAlert
-      type="error"
+      type="warning"
       className="mt-16"
       centered={!canWithdraw}
       showIcon
@@ -23,7 +26,10 @@ export const UnderConstructionAlert = () => {
         <Text className="text-sm">
           The agent is temporarily disabled due to technical issues until
           further notice.{' '}
-          {canWithdraw && `You can withdraw your funds anytime.`}
+          {canWithdraw &&
+            (hasExternalFunds
+              ? 'You can start your agent to withdraw its funds at any time.'
+              : `You can withdraw your funds anytime.`)}
         </Text>
       }
       action={
