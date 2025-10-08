399b327a06833e926320d098c32a63178f1cd22b
mohandast52 <mohandast52@gmail.com>
2025-10-08 16:17:28 +0530

    fix: update hash for PredictTrader agent template

--- frontend/constants/serviceTemplates.ts
+++ frontend/constants/serviceTemplates.ts
@@ -17,7 +17,7 @@ export const KPI_DESC_PREFIX = '[Pearl service]';
 export const PREDICT_SERVICE_TEMPLATE: ServiceTemplate = {
   agentType: AgentType.PredictTrader, // TODO: remove if causes errors on middleware
   name: 'Trader Agent', // should be unique across all services and not be updated
-  hash: 'bafybeig7ljclqteme5yi5vigumk62o3wyartxvwu3rhw3pl46selr6fl5y',
+  hash: 'bbafybeih2lys5njpduxootiq4epkxn2kajo2767bvcbosyhhebi7jazju6i',
   description: `${KPI_DESC_PREFIX} Trader agent for omen prediction markets`,
   image:
     'https://operate.olas.network/_next/image?url=%2Fimages%2Fprediction-agent.png&w=3840&q=75',
