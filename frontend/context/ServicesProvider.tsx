396a034ded6d67b50dbad81cc54064aa8f18e744
Rajat Verma <rajatverma5885045@gmail.com>
2025-10-14 14:12:19 +0530

    fix: support multiple agents per chain (#1240)
    * feat: add types
    
    * feat: update middleware version
    
    * feat: switch to service_public_id
    
    * fix: add missing public Ids
    
    * fix: add supported agents filter for staking programs
    
    * fix: add home chain filter as well
    
    * fix: incomplete setup (backup wallet not set) (#1193)
    
    * fix: incomplete account setup: backup wallet not set
    
    * fix: beta related changes
    
    * fix: code review comments
    
    * fix: code review comment
    
    * fix: add comment to describe fields
    
    * fix: improvise comment

--- frontend/context/ServicesProvider.tsx
+++ frontend/context/ServicesProvider.tsx
@@ -272,7 +272,8 @@ export const ServicesProvider = ({ children }: PropsWithChildren) => {
     if (isNilOrEmpty(services)) return;
 
     const currentService = services.find(
-      ({ home_chain }) =>
+      ({ service_public_id, home_chain }) =>
+        service_public_id === selectedAgentConfig.servicePublicId &&
         home_chain === selectedAgentConfig.middlewareHomeChainId,
     );
     if (!currentService) {
