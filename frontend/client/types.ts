6888e59526ec9709c41cf82b114cfbcd94b89f78
Rajat Verma <rajatverma5885045@gmail.com>
2025-10-14 14:12:01 +0530

    fix: improvise comment

--- frontend/client/types.ts
+++ frontend/client/types.ts
@@ -64,14 +64,16 @@ type EnvVariableAttributes = {
 
 export type MiddlewareServiceResponse = {
   /**
-   * Field to address agent's uniqueness, eg: optimus, trader, etc.
+   * Addresses agent's uniqueness, eg: optimus, trader, etc.
    * It is unique per chain, but can have the same value if the same agent
    * (with different config) is used across different chains. For eg: Optimus and Modius
-   * have the same service_public_id
+   * have the same service_public_id.
+   * In order to define an agent uniqueneess, we should check for the combination of
+   * service_public_id and home_chain. As no two agents would have the same values for these fields.
    */
   service_public_id: string;
   /**
-   * Field to address config's uniqueness.
+   * Addresses config's uniqueness.
    * The same agent can have different configs, and so this field will have different values.
    * eg: Optimus and Modius are the same agents (as per the logic/code), but their configs would
    * be different, hence they will have difference values of service_config_id
