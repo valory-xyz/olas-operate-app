0072532f42641523cca08c8c8a0dad1e9518f26f
mohandast52 <mohandast52@gmail.com>
2025-10-17 14:20:54 +0530

    refactor: update useSetupTrayIcon hook to use balance and refill requirements context

--- frontend/components/MainPageV1/hooks/useSetupTrayIcon.tsx
+++ frontend/components/MainPageV1/hooks/useSetupTrayIcon.tsx
@@ -2,29 +2,30 @@ import { useEffect } from 'react';
 
 import { MiddlewareDeploymentStatus } from '@/client';
 import {
-  useBalanceContext,
+  useBalanceAndRefillRequirementsContext,
   useElectronApi,
   useService,
   useServices,
 } from '@/hooks';
 
 export const useSetupTrayIcon = () => {
-  const { isLowBalance } = useBalanceContext();
+  const { setTrayIcon } = useElectronApi();
   const { selectedService } = useServices();
   const { deploymentStatus } = useService(selectedService?.service_config_id);
-  const { setTrayIcon } = useElectronApi();
+  const { isPearlWalletRefillRequired } =
+    useBalanceAndRefillRequirementsContext();
 
   useEffect(() => {
-    if (isLowBalance) {
-      setTrayIcon?.('low-gas');
-    } else if (deploymentStatus === MiddlewareDeploymentStatus.DEPLOYED) {
-      setTrayIcon?.('running');
-    } else if (deploymentStatus === MiddlewareDeploymentStatus.STOPPED) {
-      setTrayIcon?.('paused');
-    } else if (deploymentStatus === MiddlewareDeploymentStatus.BUILT) {
-      setTrayIcon?.('logged-out');
-    }
-  }, [isLowBalance, deploymentStatus, setTrayIcon]);
+    if (!setTrayIcon) return;
 
-  return null;
+    if (isPearlWalletRefillRequired) {
+      setTrayIcon('low-gas');
+    } else if (deploymentStatus === MiddlewareDeploymentStatus.DEPLOYED) {
+      setTrayIcon('running');
+    } else if (deploymentStatus === MiddlewareDeploymentStatus.STOPPED) {
+      setTrayIcon('paused');
+    } else if (deploymentStatus === MiddlewareDeploymentStatus.BUILT) {
+      setTrayIcon('logged-out');
+    }
+  }, [isPearlWalletRefillRequired, deploymentStatus, setTrayIcon]);
 };
