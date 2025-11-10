74f83d94a61f3f676493daa9406e68162ab14041
Atatakai <densatsu@mail.ru>
2025-11-10 17:53:42 +0100

    fix: show error if exceeds max number of files or max file size for support uploads

--- frontend/components/SupportModal/FileUpload.tsx
+++ frontend/components/SupportModal/FileUpload.tsx
@@ -1,5 +1,5 @@
-import { Button, Flex, Typography, Upload, UploadFile } from 'antd';
-import { UploadChangeParam } from 'antd/es/upload';
+import { Button, Flex, message, Typography, Upload, UploadFile } from 'antd';
+import { RcFile, UploadChangeParam } from 'antd/es/upload';
 import { TbCloudUpload, TbPaperclip, TbTrash } from 'react-icons/tb';
 
 import { COLOR } from '@/constants/colors';
@@ -8,13 +8,9 @@ import { formatFileSize } from './utils';
 
 const { Text } = Typography;
 
-const DRAGGER_STYLES: React.CSSProperties = {
-  backgroundColor: COLOR.BACKGROUND,
-  padding: 8,
-  border: `1px dashed ${COLOR.GRAY_3}`,
-};
-
-const ACCEPTED_UPLOAD_ACCEPT = [
+const MAX_FILES = 5;
+const MAX_FILE_SIZE_MB = 4.5;
+const ACCEPTED_FILE_TYPES = [
   'image/*',
   'video/*',
   '.zip',
@@ -22,18 +18,51 @@ const ACCEPTED_UPLOAD_ACCEPT = [
   'application/x-zip-compressed',
 ].join(',');
 
+/**
+ * Validate files before adding to the upload list.
+ * - Returning Upload.LIST_IGNORE prevents the file from being added.
+ * - Returning false allows adding.
+ */
+const beforeUpload = (
+  file: RcFile,
+  addedFileList: RcFile[],
+  currentFileList: UploadFile[],
+) => {
+  const totalFiles = addedFileList.length + currentFileList.length;
+  const fileSizeMB = file.size / 1024 / 1024;
+
+  if (totalFiles > MAX_FILES) {
+    message.error({ content: 'Too Many Files', key: 'max-files-error' });
+    return Upload.LIST_IGNORE;
+  }
+
+  if (fileSizeMB > MAX_FILE_SIZE_MB) {
+    message.error({ content: 'File Too Large', key: 'file-size-error' });
+    return Upload.LIST_IGNORE;
+  }
+
+  return false;
+};
+
+const DRAGGER_STYLES: React.CSSProperties = {
+  backgroundColor: COLOR.BACKGROUND,
+  padding: 8,
+  border: `1px dashed ${COLOR.GRAY_3}`,
+};
+
 type FileUploadProps = {
   onChange?: (info: UploadChangeParam<UploadFile>) => void;
   fileList?: UploadFile[];
 };
-
 const FileUpload = ({ onChange, fileList }: FileUploadProps) => {
   return (
     <Upload.Dragger
       name="files"
       multiple
-      accept={ACCEPTED_UPLOAD_ACCEPT}
-      beforeUpload={() => false}
+      accept={ACCEPTED_FILE_TYPES}
+      beforeUpload={(file, addedFileList) =>
+        beforeUpload(file, addedFileList, fileList || [])
+      }
       showUploadList={false}
       onChange={onChange}
       fileList={fileList}
@@ -45,7 +74,7 @@ const FileUpload = ({ onChange, fileList }: FileUploadProps) => {
           Upload screenshots of the issue
         </Text>
         <Text className="text-neutral-tertiary text-xs">
-          Max 5 files, 4.5MB each.
+          Max {MAX_FILES} files, {MAX_FILE_SIZE_MB}MB each.
         </Text>
       </Flex>
     </Upload.Dragger>
