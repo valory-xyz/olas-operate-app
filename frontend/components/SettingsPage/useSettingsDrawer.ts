20e1b15e3a9ad631d39a91e44d2563362c0851d4
atepem <57289722+atepem@users.noreply.github.com>
2025-11-12 16:09:38 +0000

    chore: fix SETTING_QUERY_KEY error

--- frontend/components/SettingsPage/useSettingsDrawer.ts
+++ frontend/components/SettingsPage/useSettingsDrawer.ts
@@ -5,7 +5,7 @@ import { SettingsService } from '@/service/Settings';
 
 export const useSettingsDrawer = () => {
   return useQuery({
-    queryKey: [REACT_QUERY_KEYS.SETTING_DRAWER_QUERY_KEY],
+    queryKey: [REACT_QUERY_KEYS.SETTING_QUERY_KEY],
     queryFn: ({ signal }) => SettingsService.getSettings(signal),
     staleTime: Infinity,
     retry: 3,
