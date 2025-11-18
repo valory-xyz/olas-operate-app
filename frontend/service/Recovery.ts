889139c8ac8a5f2c111c7ccb13dabfd39c5b90b5
mohandast52 <mohandast52@gmail.com>
2025-11-18 18:09:20 +0530

    refactor: remove debug code and streamline getExtendedWallet function

--- frontend/service/Recovery.ts
+++ frontend/service/Recovery.ts
@@ -1,21 +1,13 @@
 import { BACKEND_URL, CONTENT_TYPE_JSON_UTF8 } from '@/constants';
 import { ExtendedWallet } from '@/types/Recovery';
 
-import extendedWalletResponse from './Recovery.json';
-
-const isDebug = false;
-const fakeExtendedWalletResponse = Promise.resolve(
-  extendedWalletResponse as ExtendedWallet[],
-);
-
 /**
  * Extended wallet information including safes and additional metadata.
  */
 const getExtendedWallet = async (
   signal?: AbortSignal,
-): Promise<ExtendedWallet[]> => {
-  if (isDebug) return fakeExtendedWalletResponse;
-  return fetch(`${BACKEND_URL}/extended/wallet`, {
+): Promise<ExtendedWallet[]> =>
+  fetch(`${BACKEND_URL}/extended/wallet`, {
     method: 'GET',
     headers: { ...CONTENT_TYPE_JSON_UTF8 },
     signal,
@@ -23,7 +15,6 @@ const getExtendedWallet = async (
     if (res.ok) return res.json();
     throw new Error('Failed to fetch extended wallet');
   });
-};
 
 export const RecoveryService = {
   getExtendedWallet,
