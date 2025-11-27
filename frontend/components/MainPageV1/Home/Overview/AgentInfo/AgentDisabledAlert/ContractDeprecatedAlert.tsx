b2adb51c3c125d9152462b6644289c6194a8ee36
atepem <57289722+atepem@users.noreply.github.com>
2025-11-27 22:33:53 +0000

    feat: add deprecated staking contract alert

---
+++ frontend/components/MainPageV1/Home/Overview/AgentInfo/AgentDisabledAlert/ContractDeprecatedAlert.tsx
@@ -0,0 +1,38 @@
+import { Button, Typography } from 'antd';
+
+import { Alert } from '@/components/ui';
+import { Pages } from '@/enums';
+import { usePageState, useStakingProgram } from '@/hooks';
+
+const { Text } = Typography;
+
+export const ContractDeprecatedAlert = () => {
+  const { goto } = usePageState();
+  const { selectedStakingProgramMeta } = useStakingProgram();
+
+  const handleSwitchContract = () => {
+    goto(Pages.SelectStaking);
+  };
+
+  return (
+    <Alert
+      showIcon
+      className="mt-16"
+      type="error"
+      message={
+        <>
+          <Text className="text-sm font-weight-500">
+            {selectedStakingProgramMeta?.name || 'Staking'} contract is
+            deprecated
+          </Text>
+          <Text className="text-sm flex mt-4 mb-8">
+            Switch to one of the available contracts to start your agent.
+          </Text>
+          <Button size="small" onClick={handleSwitchContract}>
+            Complete Setup
+          </Button>
+        </>
+      }
+    />
+  );
+};
