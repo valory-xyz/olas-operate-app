2bf5f94d2e13d9c5445c8dd1fb13c76be92c4e48
mohandast52 <mohandast52@gmail.com>
2025-11-28 18:51:42 +0530

    refactor: remove unused types and imports from Records.ts

--- frontend/types/Records.ts
+++ frontend/types/Records.ts
@@ -1,13 +1,4 @@
-import { TokenSymbolMap } from '@/constants';
-
 import { Address } from './Address';
 
 export type AddressNumberRecord = Record<Address, number>;
-export type AddressBooleanRecord = Record<Address, boolean>;
 export type AddressTxnRecord = Record<Address, `0x${string}`>;
-
-// defines token balances in a wallet by token name
-export type WalletAddressNumberRecord = Record<
-  Address,
-  Record<typeof TokenSymbolMap.ETH | typeof TokenSymbolMap.OLAS, number>
->;
