fec089554a417692da091a364a7962dfe1e286d7
atepem <57289722+atepem@users.noreply.github.com>
2025-12-01 23:52:41 +0000

    style: add contact support option to errors

--- frontend/components/SettingsPage/index.tsx
+++ frontend/components/SettingsPage/index.tsx
@@ -19,6 +19,7 @@ import {
 } from '@/hooks';
 import { Address, Optional } from '@/types';
 
+import { OnRampPaymentSteps } from '../SetupPage/Create/SetupOnRamp/OnRampPaymentSteps/OnRampPaymentSteps';
 import { RecoveryModal } from './RecoveryModal';
 import { YourFundsAtRiskAlert } from './YourFundsAtRiskAlert';
 
@@ -179,6 +180,8 @@ const SettingsMain = () => {
             <Text style={{ lineHeight: 1 }}>••••••••••••••••••••</Text>
           </Flex>
         </CardSection>
+
+        <OnRampPaymentSteps onRampChainId={100} />
 
         {hideWallet ? null : (
           <CardSection $padding="24px" $borderBottom vertical>
