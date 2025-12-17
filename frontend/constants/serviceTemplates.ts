d3bdbe97f126a5426a38e0d67618fa898bfc6688
Atatakai <densatsu@mail.ru>
2025-12-17 23:13:55 +0100

    chore: bump agent release

--- frontend/constants/serviceTemplates.ts
+++ frontend/constants/serviceTemplates.ts
@@ -627,7 +627,7 @@ export const PETT_AI_SERVICE_TEMPLATE: ServiceTemplate = {
     repository: {
       owner: 'valory-xyz',
       name: 'pettai-agent',
-      version: 'v0.0.25',
+      version: 'v0.0.26',
     },
   },
   home_chain: MiddlewareChainMap.BASE,
