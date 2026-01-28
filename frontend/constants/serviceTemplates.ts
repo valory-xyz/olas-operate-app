072d36998670f6aa94a0b76785f485623f5515c1
DivyaNautiyal07 <divyanautiyal07@gmail.com>
2026-01-28 20:43:02 +0530

    chore: update hash

--- frontend/constants/serviceTemplates.ts
+++ frontend/constants/serviceTemplates.ts
@@ -22,17 +22,17 @@ export const KPI_DESC_PREFIX = '[Pearl service]';
 export const PREDICT_SERVICE_TEMPLATE: ServiceTemplate = {
   agentType: AgentMap.PredictTrader,
   name: 'Trader Agent', // should be unique across all services and not be updated
-  hash: 'bafybeie5olhoysjnthubtpluil64lagz4z5dkqoubl6n6c7c6cayrqmxxq',
+  hash: 'bafybeifsip5gf2tlf3sstk6jv3lognaubk4dffbw2nv42ojr7q7osjjqkm',
   description: `${KPI_DESC_PREFIX} Trader agent for omen prediction markets`,
   image:
     'https://operate.olas.network/_next/image?url=%2Fimages%2Fprediction-agent.png&w=3840&q=75',
-  service_version: 'v0.29.1-rc5',
+  service_version: 'v0.29.1',
   agent_release: {
     is_aea: true,
     repository: {
       owner: 'valory-xyz',
       name: 'trader',
-      version: 'v0.29.1-rc5',
+      version: 'v0.29.1',
     },
   },
   home_chain: MiddlewareChainMap.GNOSIS,
