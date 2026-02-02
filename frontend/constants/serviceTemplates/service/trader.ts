7d2ad6481c06dc256d1161b432d7f1a835805abc
Jenslee Dsouza <dsouzajenslee@gmail.com>
2026-02-02 17:56:53 +0530

    chore: bump to fixed polystrat

--- frontend/constants/serviceTemplates/service/trader.ts
+++ frontend/constants/serviceTemplates/service/trader.ts
@@ -152,14 +152,14 @@ export const PREDICT_SERVICE_TEMPLATE: ServiceTemplate = {
 } as const;
 
 export const PREDICT_POLYMARKET_SERVICE_TEMPLATE: ServiceTemplate = {
-  hash: 'bafybeihphgq4ydmaep3xxnjtujwex33cmvkwbhej3v6v7d2c6rasx34hzq',
-  service_version: 'v0.31.0-rc6',
+  hash: 'bafybeicita57k6hm5hvyy2ge3lqkfloigmhat3flkzpny2rzavqe2lztbe',
+  service_version: 'v0.31.0-rc7',
   agent_release: {
     is_aea: true,
     repository: {
       owner: 'valory-xyz',
       name: 'trader',
-      version: 'v0.31.0-rc6',
+      version: 'v0.31.0-rc7',
     },
   },
   agentType: AgentMap.Polystrat,
