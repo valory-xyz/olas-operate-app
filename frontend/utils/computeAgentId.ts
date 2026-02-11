758c0eb715e51a5cc77afe5e0979ca1de201deb0
mohandast52 <mohandast52@gmail.com>
2026-02-12 02:10:32 +0530

    refactor: remove NAMESPACE constant from computeAgentId utility

--- frontend/utils/computeAgentId.ts
+++ frontend/utils/computeAgentId.ts
@@ -3,13 +3,8 @@ import { utils } from 'ethers';
 const abi = new utils.AbiCoder();
 
 /**
- * Matches: bytes32 constant NAMESPACE = keccak256("Olas");
- */
-export const NAMESPACE: string = utils.keccak256(utils.toUtf8Bytes('Olas')); // 0x + 64 hex chars
-
-/**
  * Matches:
- * keccak256(abi.encode(NAMESPACE, chainId, serviceRegistryContract, tokenId))
+ * keccak256(abi.encode(chainId, serviceRegistryContract, tokenId))
  *
  * NOTE: This is abi.encode (NOT encodePacked), so we use AbiCoder.encode.
  */
