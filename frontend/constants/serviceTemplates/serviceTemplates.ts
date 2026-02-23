853bb738016f513534646806c86113528e2f4739
Atatakai <densatsu@mail.ru>
2026-02-23 15:41:52 +0100

    chore: update pettai version

--- frontend/constants/serviceTemplates/serviceTemplates.ts
+++ frontend/constants/serviceTemplates/serviceTemplates.ts
@@ -163,13 +163,13 @@ export const PETT_AI_SERVICE_TEMPLATE: ServiceTemplate = {
   description: 'Pett.ai autonomous agent service for virtual pet management.',
   image:
     'https://gateway.autonolas.tech/ipfs/QmQYDGMg8m91QQkTWSSmANs5tZwKrmvUCawXZfXVVWQPcu',
-  service_version: 'v0.1.6',
+  service_version: 'v0.1.7',
   agent_release: {
     is_aea: false,
     repository: {
       owner: 'valory-xyz',
       name: 'pettai-agent',
-      version: 'v0.1.6',
+      version: 'v0.1.7',
     },
   },
   home_chain: MiddlewareChainMap.BASE,
