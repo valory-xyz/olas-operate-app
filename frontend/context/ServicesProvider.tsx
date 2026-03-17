1900cc3e89216025f5600528ecd17976419df38b
Atatakai <densatsu@mail.ru>
2026-03-17 15:02:11 +0100

    chore: move state up for readability

--- frontend/context/ServicesProvider.tsx
+++ frontend/context/ServicesProvider.tsx
@@ -194,6 +194,13 @@ export const ServicesProvider = ({ children }: PropsWithChildren) => {
     refetchInterval: FIFTEEN_SECONDS_INTERVAL,
   });
 
+  // Stores temporary overrides for service statuses to avoid UI glitches.
+  // Right after updating the status on the backend, initial queries
+  // might return outdated or incorrect value
+  const [serviceStatusOverrides, setServiceStatusOverrides] = useState<
+    Record<string, Maybe<MiddlewareDeploymentStatus>>
+  >({});
+
   const { data: allDeployments, isLoading: isAllDeploymentsLoading } = useQuery(
     {
       queryKey: REACT_QUERY_KEYS.ALL_SERVICE_DEPLOYMENTS_KEY,
@@ -223,13 +230,6 @@ export const ServicesProvider = ({ children }: PropsWithChildren) => {
     : undefined;
 
   const isSelectedServiceDeploymentStatusLoading = isAllDeploymentsLoading;
-
-  // Stores temporary overrides for service statuses to avoid UI glitches.
-  // Right after updating the status on the backend, initial queries
-  // might return outdated or incorrect value
-  const [serviceStatusOverrides, setServiceStatusOverrides] = useState<
-    Record<string, Maybe<MiddlewareDeploymentStatus>>
-  >({});
 
   const selectedService = useMemo<Service | undefined>(() => {
     if (!services) return;
