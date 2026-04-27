34027fe7eded554ac9242dc14e4ee0799f4b0393
Jenslee Dsouza <dsouzajenslee@gmail.com>
2026-04-27 19:07:24 +0530

    chore: bump polystart

--- frontend/constants/serviceTemplates/service/trader.ts
+++ frontend/constants/serviceTemplates/service/trader.ts
@@ -151,14 +151,14 @@ export const PREDICT_SERVICE_TEMPLATE: ServiceTemplate = {
 } as const;
 
 export const PREDICT_POLYMARKET_SERVICE_TEMPLATE: ServiceTemplate = {
-  hash: 'bafybeiaw7pugbxrcqtsiogmoa2psgw4svngkasc62rcjhiftq3blxo3wiq',
-  service_version: 'v0.35.0-rc1',
+  hash: 'bafybeicewr36eycfxfage6onhdh46w2y37hw3bdwktohajrvhl4bkwgvmm',
+  service_version: 'v0.35.0-rc2',
   agent_release: {
     is_aea: true,
     repository: {
       owner: 'valory-xyz',
       name: 'trader',
-      version: 'v0.35.0-rc1',
+      version: 'v0.35.0-rc2',
     },
   },
   agentType: AgentMap.Polystrat,
