1b5a03ad451a8367890341b79ff7c42f01921b41
jmoreira-valory <jose.moreira.sanchez@valory.xyz>
2026-05-22 10:29:39 +0200

    feat: update omenstrat

--- frontend/constants/serviceTemplates/service/trader.ts
+++ frontend/constants/serviceTemplates/service/trader.ts
@@ -11,14 +11,14 @@ import { X402_ENABLED_FLAGS } from '../../x402';
 import { KPI_DESC_PREFIX } from '../constants';
 
 export const PREDICT_SERVICE_TEMPLATE: ServiceTemplate = {
-  hash: 'bafybeigjqjw5kpaoire7jp3n7dbdzejovvcpnfrroecrhqgwkg3x7o4noi',
-  service_version: 'v0.38.3-rc1',
+  hash: 'bafybeibvy2kw6mkn567yojpdyywu5jwg7rshpuivsxuxig6puag52q2zny',
+  service_version: 'v0.38.7-rc1',
   agent_release: {
     is_aea: true,
     repository: {
       owner: 'valory-xyz',
       name: 'trader',
-      version: 'v0.38.3-rc1',
+      version: 'v0.38.7-rc1',
     },
   },
   agentType: AgentMap.PredictTrader,
