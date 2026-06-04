c782dc08e2f14ad54d9e0671034332d0a0139a7c
OjusWiZard <ojuswimail@gmail.com>
2026-06-04 19:34:02 +0530

    chore: bump polystrat to v0.39.0-rc2

--- frontend/constants/serviceTemplates/service/trader.ts
+++ frontend/constants/serviceTemplates/service/trader.ts
@@ -144,14 +144,14 @@ export const PREDICT_SERVICE_TEMPLATE: ServiceTemplate = {
 } as const;
 
 export const PREDICT_POLYMARKET_SERVICE_TEMPLATE: ServiceTemplate = {
-  hash: 'bafybeifpkmu3i6ujf4pgasulnmvmlvfrea5heschwf2qireyvod247snq4',
-  service_version: 'v0.39.0-rc1',
+  hash: 'bafybeicpx5xfkpkbxhqavkpgi7u6x5lmhabjcrxkvacsqdxyezgp4zqxcy',
+  service_version: 'v0.39.0-rc2',
   agent_release: {
     is_aea: true,
     repository: {
       owner: 'valory-xyz',
       name: 'trader',
-      version: 'v0.39.0-rc1',
+      version: 'v0.39.0-rc2',
     },
   },
   agentType: AgentMap.Polystrat,
